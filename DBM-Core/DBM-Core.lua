086e3d6421944f7699d6eacf04920282a5c325ad
Aaron <evishit@gmail.com>
2021-01-11 20:34:23 +0000

    version bump

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -58,10 +58,10 @@ f:SetScript("OnUpdate", fCLFix)
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 5030 $"):sub(12, -3),
-	Version = "5.003",
-	DisplayVersion = "5.003 by Gomi/House", -- the string that is shown as version
-	ReleaseRevision = 50030 -- the revision of the latest stable version that is available (for /dbm ver2)
+	Revision = ("$Revision: 5.004 $"):sub(12, -3),
+	Version = "5.004",
+	DisplayVersion = "5.004 by Gomi/House", -- the string that is shown as version
+	ReleaseRevision = 50040 -- the revision of the latest stable version that is available (for /dbm ver2)
 }
 
 DBM_SavedOptions = {}
